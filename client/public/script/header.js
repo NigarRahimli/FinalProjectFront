1705c3d7c3df2a4899cf5326c3655858f5df53d6
NigarRahimli <nigarrahimli07@gmail.com>
2023-11-02 15:50:47 +0400

    register and login is ready

---
+++ client/public/script/header.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector(".nav-menu__side__burger");
+const body = document.querySelector("body");
+const side = document.getElementsByClassName("nav-menu__side__list")[0];
+burger.addEventListener("click", (event) => {
+  event.stopPropagation();
+  side.style.right = "0";
+});
+body.addEventListener("click", () => {
+  side.style.right = "-100%";
+});
+side.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1050) {
+    side.style.right = "-100%";
+  }
+});
