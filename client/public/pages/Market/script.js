8e2529d28f87a6c0293480f59cdea65f51e5c89b
NigarRahimli <nigarrahimli07@gmail.com>
2023-11-18 15:44:21 +0400

    market is ready fully

--- client/public/pages/Market/script.js
+++ client/public/pages/Market/script.js
@@ -1,17 +1,17 @@
 const cardContainer = document.querySelector(".discover__cards");
 const searchInput = document.getElementsByClassName("search__bar__input")[0];
 const loadMoreButton = document.querySelector(".load-more");
-const loadingCards = document.getElementsByClassName("card is-loading");
+let loadingCards = document.getElementsByClassName("card is-loading");
 let hasMore = true;
 const loadingCardHTML = `
-  <div class="card is-loading">
-    <div class="image"></div>
-    <div class="content">
-      <h2></h2>
-      <p></p>
-    </div>
-  </div>
-`;
+   
+        <div class="image"></div>
+        <div class="content">
+          <h2></h2>
+          <p></p>
+        </div>
+      
+    `;
 
 let skip = 0;
 let skipStr = 0;
@@ -27,9 +27,7 @@ function getNfts() {
 }
 
 function fetchNfts(skip) {
-
-
-
+  loadMoreButton.disabled = true;
   appendLoadingCards();
 
   fetch(`http://localhost:3000/api/nfts`, {
@@ -49,26 +47,31 @@ function fetchNfts(skip) {
       fillArtistPage(data);
 
       hasMore = data.hasMore;
-
+      if (!hasMore) {
+        loadMoreButton.disabled = true;
+      } else {
+        loadMoreButton.disabled = false;
+      }
     })
-    .finally(() => {
-     
-    });
+    .finally(() => {});
 }
 
 let id = 0;
 
 searchInput.addEventListener("keyup", (e) => {
+  skipStr = 0;
+  skip=0;
   clearTimeout(id);
   id = setTimeout(() => {
-    searchString = e.target.value;
+    searchString = e.target.value.trim();
+    cardContainer.innerHTML = "";
+
     fetchNftsforString(skipStr, searchString);
   }, 2000);
 });
 
 function fetchNftsforString(skip, searchString) {
-
-  cardContainer.innerHTML = "";
+  loadMoreButton.disabled = true;
 
   appendLoadingCards();
 
@@ -85,15 +88,20 @@ function fetchNftsforString(skip, searchString) {
   })
     .then((res) => res.json())
     .then((data) => {
-      // Remove loading cards
       removeLoadingCards();
 
-      cardContainer.innerHTML = "";
+      // cardContainer.innerHTML = "";
       fillArtistPage(data);
+      hasMore = data.hasMore;
+      if (hasMore) {
+        loadMoreButton.disabled = false;
+      } else {
+        loadMoreButton.disabled = true;
+      }
     })
-    .finally(() => {
-    
-    });
+    .finally(() => {});
+  skipStr += pageSize;
+  console.log(skipStr);
 }
 
 function fillArtistPage(data) {
@@ -105,25 +113,25 @@ function fillArtistPage(data) {
     const card = document.createElement("a");
     card.classList.add("discover__cards__card");
     card.innerHTML += `
-      <img class="discover__cards__card__img" src="../../../../${nft.imgPath}" alt="" />
-      <div class="discover__cards__card__content">
-        <h5 class="heading-fifth-work">${nft.name}</h5>
-        <div class="discover__cards__card__content__dancer">
-          <img src="../../../../${nft.creator?.profileImgPath}" alt="" />
-          <span class="base-mono">${nft.creator?.name}</span>
-        </div>
-        <div class="discover__cards__card__content__records">
-          <div class="discover__cards__card__content__records__price">
-            <p class="caption-mono col-grey">Price</p>
-            <p class="base-mono">${nft.price?.value} ${nft.price?.currency}</p>
+          <img class="discover__cards__card__img" src="../../../../${nft.imgPath}" alt="" />
+          <div class="discover__cards__card__content">
+            <h5 class="heading-fifth-work">${nft.name}</h5>
+            <div class="discover__cards__card__content__dancer">
+              <img src="../../../../${nft.creator?.profileImgPath}" alt="" />
+              <span class="base-mono">${nft.creator?.name}</span>
+            </div>
+            <div class="discover__cards__card__content__records">
+              <div class="discover__cards__card__content__records__price">
+                <p class="caption-mono col-grey">Price</p>
+                <p class="base-mono">${nft.price?.value} ${nft.price?.currency}</p>
+              </div>
+              <div class="discover__cards__card__content__records__high">
+                <p class="caption-mono col-grey">Highest Bid</p>
+                <p class="base-mono">${nft.highestBid?.value} ${nft.highestBid?.currency}</p>
+              </div>
+            </div>
           </div>
-          <div class="discover__cards__card__content__records__high">
-            <p class="caption-mono col-grey">Highest Bid</p>
-            <p class="base-mono">${nft.highestBid?.value} ${nft.highestBid?.currency}</p>
-          </div>
-        </div>
-      </div>
-    `;
+        `;
 
     cardContainer.appendChild(card);
   });
@@ -131,26 +139,34 @@ function fillArtistPage(data) {
   skip += pageSize;
 }
 
-
-
 function appendLoadingCards() {
-
   for (let i = 0; i < pageSize; i++) {
     const loadingCard = document.createElement("div");
+    loadingCard.className = "card is-loading";
     loadingCard.innerHTML = loadingCardHTML;
     cardContainer.appendChild(loadingCard);
   }
+  loadingCards = document.getElementsByClassName("card is-loading");
 }
 
 function removeLoadingCards() {
-  Array.from(loadingCards).forEach((element) => {
+  const loadingCardsArray = Array.from(loadingCards);
+  
+  // Iterate over the array and remove each element
+  loadingCardsArray.forEach((element) => {
+    console.log("removed");
     element.remove();
   });
 }
 
 loadMoreButton.addEventListener("click", () => {
+  console.log("clicked");
   if (hasMore) {
-    fetchNfts(skip);
+    if (searchString) {
+      fetchNftsforString(skipStr, searchString);
+    } else {
+      fetchNfts(skip);
+    }
   }
 });
 
